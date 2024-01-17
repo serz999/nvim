fee14d8d0f72c498cc99a5c5bdb0d53fca2e606a
serz999 <serz99992@gmail.com>
2024-01-17 11:43:23 +0300

    feat: remove telescope-fzf-native plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,19 +1,13 @@
 return {
     'nvim-telescope/telescope.nvim',
-    version = '0.1.1',
+    tag = '0.1.5',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            build = 'make'
-        },
     },
     config = function()
         local telescope = require('telescope')
 
         telescope.setup({})
-
-        telescope.load_extension('fzf')
 
         vim.keymap.set('n', '<leader>f', '<cmd>Telescope find_files<cr>')
         vim.keymap.set('n', '<leader>g', '<cmd>Telescope live_grep<cr>')
